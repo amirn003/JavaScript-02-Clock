2fdfcdc39da731e7fc1dd730988ef2d8730c0727
amirn003 <amir.naar@gmail.com>
2023-10-25 11:03:57 +0200

    refactor minute hand

--- index.js
+++ index.js
@@ -2,22 +2,15 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand')
 
-function setSecond() {
+function setDate() {
   const now = new Date();
   const second = now.getSeconds();
-  const secondDegrees = ((second / 60) * 360 ) +90;
+  const secondDegrees = ((second / 60) * 360 ) + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const minute = now.getMinutes();
+  const minuteDegrees = ((minute / 60) * 360 ) + 90;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
   console.log("Second: ", second);
 }
-setInterval(setSecond,1000);
-
-
-function setMinute() {
-  const now = new Date();
-  const minute = now.getMinutes();
-  const minuteDegrees = ((minute / 60) * 360 ) +90;
-  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-  console.log("Minute: ", minute);
-}
-
-setInterval(setMinute,60000);
+setInterval(setDate,1000);
